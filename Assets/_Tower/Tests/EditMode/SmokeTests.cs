7e79f97a9f0a4935767d573d3c1a40e2ad8280ae
Beomsoo Kim <132043689+Muelfly@users.noreply.github.com>
2026-07-05 20:02:59 +0900

    T0: project conventions and structure (#1)

---
+++ Assets/_Tower/Tests/EditMode/SmokeTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Tower.Tests.EditMode
+{
+    public sealed class SmokeTests
+    {
+        [Test]
+        public void TestRunnerIsWired()
+        {
+            Assert.Pass();
+        }
+    }
+}
